9b7393871a4a3c080d8c6e9447c53559d27771d2
carsin <20868514+carsin@users.noreply.github.com>
2023-12-12 20:58:12 +0100

    add de/acceleration and stopping to npc movement

--- src/npc.rs
+++ src/npc.rs
@@ -7,6 +7,7 @@ use super::constants::*;
 #[derive(Component)]
 pub struct Npc {
     pub velocity: Vec2,
+    pub target_velocity: Vec2,
     pub speed: f32,
     pub health: f32,
 }
@@ -27,8 +28,8 @@ pub fn spawn_npc(commands: &mut Commands) {
         .insert(LockedAxes::ROTATION_LOCKED)
         .insert(Ccd::enabled())
         .insert(TransformBundle::from(Transform::from_xyz(
-            rng.gen_range(-GRID_SIZE / 2. + NPC_SIZE..GRID_SIZE / 2. - NPC_SIZE),
-            rng.gen_range(-GRID_SIZE / 2. + NPC_SIZE..GRID_SIZE / 2. - NPC_SIZE),
+            rng.gen_range(-GRID_SIZE / 2. + NPC_SIZE * 2.0..GRID_SIZE / 2. - NPC_SIZE * 2.),
+            rng.gen_range(-GRID_SIZE / 2. + NPC_SIZE * 2.0..GRID_SIZE / 2. - NPC_SIZE * 2.),
             100.,
         )))
         .insert(Velocity {
@@ -37,23 +38,36 @@ pub fn spawn_npc(commands: &mut Commands) {
         })
         .insert(Npc {
             velocity: Vec2::ZERO,
+            target_velocity: Vec2::ZERO,
             speed: NPC_SPEED,
             health: NPC_INITIAL_HEALTH,
         });
 }
 
-pub fn npc_movement_system(mut query: Query<(&mut Velocity, &mut Npc, &Transform)>) {
+pub fn npc_movement_system(
+    time: Res<Time>,
+    mut query: Query<(&mut Velocity, &mut Npc, &Transform)>,
+) {
+    let dt = time.delta_seconds();
+    let mut rng = rand::thread_rng();
+
     for (mut velocity, mut npc, transform) in query.iter_mut() {
-        // Randomly decide whether to change direction
-        if rand::random::<f32>() < 0.01 {
-            npc.velocity = Vec2::new(rand::random::<f32>() - 0.5, rand::random::<f32>() - 0.5)
-                .normalize_or_zero()
-                * npc.speed;
+        // Randomly decide whether to change direction or stop
+        if rng.gen::<f32>() < 0.01 {
+            let stop_moving = rng.gen_bool(0.30); // chance to stop moving
+            npc.target_velocity = if stop_moving {
+                Vec2::ZERO
+            } else {
+                Vec2::new(rng.gen_range(-1.5..1.5), rng.gen_range(-1.5..1.5))
+                    .normalize_or_zero() * npc.speed
+            };
         }
 
-        // Set the linear velocity
+        // Apply acceleration
+        npc.velocity = npc.velocity.lerp(npc.target_velocity, 5.0 * dt);
         velocity.linvel = npc.velocity;
 
+        // Clamping logic to keep NPCs within the grid
         let clamped_x = transform.translation.x.clamp(
             -GRID_SIZE / 2.0 + NPC_SIZE / 2.0,
             GRID_SIZE / 2.0 - NPC_SIZE / 2.0,
@@ -64,15 +78,11 @@ pub fn npc_movement_system(mut query: Query<(&mut Velocity, &mut Npc, &Transform
         );
 
         // Apply clamping
-        velocity.linvel.x = if clamped_x != transform.translation.x {
-            0.0
-        } else {
-            velocity.linvel.x
-        };
-        velocity.linvel.y = if clamped_y != transform.translation.y {
-            0.0
-        } else {
-            velocity.linvel.y
-        };
+        if clamped_x != transform.translation.x {
+            velocity.linvel.x = 0.0;
+        }
+        if clamped_y != transform.translation.y {
+            velocity.linvel.y = 0.0;
+        }
     }
 }
